f5c7a200ca985d868407202f3af3fd53b17418aa
oncoder88 <elensoroka88@gmail.com>
2022-09-05 15:44:27 +0100

    Changed DOM-elements according to the function

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
 const newYears = "1 Jan 2023";
 
@@ -9,8 +13,17 @@ function countdown() {
 
   const days = Math.floor(remainSeconds / 3600 / 24);
   const hours = Math.floor(remainSeconds / 3600) % 24;
-  const minutes = Math.floor(remainSeconds / 60) % 60;
+  const mins = Math.floor(remainSeconds / 60) % 60;
   const seconds = Math.floor(remainSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minEl.innerHTML = formatTime(mins);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+ return time < 10 ? (`0${time}`) : time;
 }
 
 countdown();
